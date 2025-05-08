b2b124aa081604651666de6f1a6ebab2ddb699c7
jenishk20 <kotharijenish2001@gmail.com>
2025-05-08 16:07:01 -0400

    Updating CORS

--- backend/app.js
+++ backend/app.js
@@ -10,7 +10,7 @@ const cookieParser = require("cookie-parser");
 app.use(cookieParser());
 app.use(
   cors({
-    origin: true,
+    origin: process.env.FRONTEND_URL,
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
   })
