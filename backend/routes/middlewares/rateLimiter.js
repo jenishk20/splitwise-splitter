0f19c9d67ac49b0be4fd84d9b0be5fe8f97e62d4
jenishk20 <kotharijenish2001@gmail.com>
2025-06-08 12:42:45 -0600

    Loosening login API rate limit.

--- backend/routes/middlewares/rateLimiter.js
+++ backend/routes/middlewares/rateLimiter.js
@@ -1,8 +1,8 @@
 const rateLimit = require("express-rate-limit");
 
 const loginLimiter = rateLimit({
-  windowMs: 15 * 60 * 1000,
-  max: 10,
+  windowMs: 1 * 60 * 1000,
+  max: 100,
   message: "Too many login attempts. Please try again later.",
 });
 
